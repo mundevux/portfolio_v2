d81657f9949bd60bdf68e5fb5412642c7a364c0e
MundevUX <camiloandres174@gmail.com>
2023-12-07 15:07:05 -0500

    Add font dependencies and update layout styles

---
+++ app/components/PrincipalButton.js
@@ -0,0 +1,5 @@
+import { Button } from "@nextui-org/react";
+
+export default function PrincipalButton(text) {
+  return <Button className="bg-white">{text}</Button>;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "primary-white": "#F0F2F5",
+        "primary-black": "#0D1821",
+        "gradient-one": "#B16CEA",
+        "gradient-two": "#FF5E69",
+        "gradient-three": "#FF8A56",
+        "gradient-four": "#FFA84B",
       },
     },
   },
-  plugins: [],
-}
+  darkMode: "class",
+  plugins: [nextui()],
+};
